934cd397caec6c6e69ce35984f3a0d373f806091
Lucas <lbguimaraes16@gmail.com>
2022-08-24 15:35:41 -0300

    Aula Prog 3

---
+++ src/main/java/br/edu/femass/model/Historico.java
@@ -0,0 +1,34 @@
+package br.edu.femass.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Historico {
+    private LocalDateTime data;
+    private  Double valor;
+    private  TipoLancamento tipoLancamento;
+
+    public Historico(Double valor, TipoLancamento tipoLancamento){
+        this.data = LocalDateTime.now();
+        this.valor= valor;
+        this.tipoLancamento = tipoLancamento;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public TipoLancamento getTipoLancamento() {
+        return tipoLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return data.format(DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm:ss")) + " - Valor:" + this.valor.toString() + " - " + this.tipoLancamento;
+
+    }
+}
